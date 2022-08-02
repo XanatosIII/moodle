9ab9d5ddda2efd3278b516724f4f43e43a3d7681
XanatosIII <100892075+XanatosIII@users.noreply.github.com>
2022-08-02 14:25:52 -0400

    Update config.php

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = getenv('MySQL');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = getenv('sql5.freemysqlhosting.net');
+$CFG->dbname    = getenv('sql5510406');
+$CFG->dbuser    = getenv('sql5510406');
+$CFG->dbpass    = getenv('rXbevsqra9');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
